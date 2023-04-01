8774e5f7af0d90447413114a657929f325698098
Solod Sergey <104456968+Solod-S@users.noreply.github.com>
2023-04-01 17:50:26 +0300

    getPopularRecipes
    - return sorted by popularity array with recipes

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 // const auth = require("../../middlewares/auth");
 const { recipes: ctrl } = require("../../controllers/");
 
+// router.get("/popular", auth, ctrl.getPopularRecipes);
+router.get("/popular", ctrl.getPopularRecipes);
+
 // router.get("/:id", auth, ctrl.getRecipeById);
 router.get("/:id", ctrl.getRecipeById);
+
 module.exports = router;
