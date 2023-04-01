ffa60cbcea02191599944e6b6b4e8ef8156845e4
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2023-04-01 13:49:20 +0300

    auth (login, register, logout)

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require("express")
-const logger = require("morgan")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
-const mainRouter = require("./routes/api/main")
-// const usersRouter = require("./routes/api/auth")
+// const mainRouter = require("./routes/api/main");
+const usersRouter = require("./routes/api/auth");
 // const ingredientsRouter = require("./routes/api/ingredients")
 // const recipesRouter = require("./routes/api/recipes")
 // const ownRecipesRouter = require("./routes/api/ownRecipes")
 // const favoriteRouter = require("./routes/api/favorite")
 // const shoppingListRouter = require("./routes/api/shoppingList")
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short"
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use("/", mainRouter)
-// app.use("/auth", usersRouter)
+// app.use("/", mainRouter);
+app.use("/auth", usersRouter);
 // app.use("/recipes", recipesRouter)
 // app.use("/ingredients", ingredientsRouter)
 // app.use("/own-recipes", ownRecipesRouter)
@@ -28,13 +28,13 @@ app.use("/", mainRouter)
 // app.use("/shopping-list", shoppingListRouter)
 
 app.use((req, res) => {
-    res.status(404).json({ message: "Not found" })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-    console.log(err)
-    const { status = 500, message = "Server error" } = err
-    res.status(status).json({ message })
-})
+  console.log(err);
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
-module.exports = app
+module.exports = app;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,29 @@
-// const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-// const { User } = require("../models/users");
+const { User } = require("../models/users");
 
-// const { HttpError } = require("../routes/errors/HttpErrors")
+const { HttpError } = require("../routes/errors/HttpErrors");
 
-// const { SECRET_KEY } = process.env;
+const { SECRET_KEY } = process.env;
+
+const authenticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(HttpError(401));
+  }
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user || !user.token || user.token !== token) {
+      next(HttpError(401, " User not found"));
+    }
+    req.user = user;
+    console.log(user);
+    next();
+  } catch {
+    next(HttpError(401));
+  }
+};
+
+module.exports = authenticate;
