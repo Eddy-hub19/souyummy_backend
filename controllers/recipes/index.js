7662ad65dea17b8484b7e4934b4e434e9b8d74cb
Marina <sheremetamaryna@gmail.com>
2023-04-01 23:17:41 +0200

    recepies/category-list
    added router.get("/category-list")

--- controllers/recipes/index.js
+++ controllers/recipes/index.js
@@ -1,10 +1,11 @@
 const ctrlWrapper = require("../ctrlWrapper");
-// const getCategories = require("./getCategories");
+const getCategories = require("./getCategories");
 const getRecipeById = require("./getRecipeById");
 const getPopularRecipes = require("./getPopularRecipes");
 
+
 module.exports = {
-  //   getCategories: ctrlWrapper(getCategories),
+    getCategories: ctrlWrapper(getCategories),
   getRecipeById: ctrlWrapper(getRecipeById),
   getPopularRecipes: ctrlWrapper(getPopularRecipes),
 };
--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -5,6 +5,9 @@ const { recipes: ctrl } = require("../../controllers/");
 // router.get("/popular", auth, ctrl.getPopularRecipes);
 router.get("/popular", ctrl.getPopularRecipes);
 
+// router.get("/category-list", auth, ctrl.getCategories);
+router.get("/category-list", ctrl.getCategories);
+
 // router.get("/:id", auth, ctrl.getRecipeById);
 router.get("/:id", ctrl.getRecipeById);
 
