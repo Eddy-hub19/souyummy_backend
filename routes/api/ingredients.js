3386f99d17ff751a37698378537f13627495efe1
EduardSekan <sekaneduard@gmail.com>
2023-04-02 18:30:25 +0300

    add func searchByIngradient

--- routes/api/ingredients.js
+++ routes/api/ingredients.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
-const ingredientList = require("../../controllers/ingredients/");
+const { ingredientList, searchByIngredients } = require("../../controllers/ingredients/");
 
 const router = express.Router();
 
 router.get("/list", ingredientList);
-// router.get("/recipes/:id/ingredients/", ingredientList);
+router.get("/recipes/search", searchByIngredients);
 
 module.exports = router;
