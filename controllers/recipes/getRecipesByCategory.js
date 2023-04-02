ec6fe961d039d653761338b199f47fbb76c33081
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-04-02 23:14:25 +0300

    finish

--- controllers/recipes/getRecipesByCategory.js
+++ controllers/recipes/getRecipesByCategory.js
@@ -1,11 +1,12 @@
 const { Recipe } = require("../../models/");
 
+
 const getRecipesByCategory = async (req, res) => {
     const { categoryName } = req.params;
-    console.log(categoryName);
-    const result = await Recipe.find({ category: categoryName }, null, { limit: 8 });
+   
+    const result = await Recipe.find({ category: categoryName}, null, { limit: 8 });
 
-res.status(200).json(result);
+    res.status(200).json(result);
 }
 
 module.exports = getRecipesByCategory;
--- controllers/recipes/index.js
+++ controllers/recipes/index.js
@@ -3,13 +3,16 @@ const getCategories = require("./getCategories");
 const getRecipeById = require("./getRecipeById");
 const getPopularRecipes = require("./getPopularRecipes");
 const getMainPageRecipes = require("./getMainPageRecipes");
-const getRecipesByName = require("./getRecipesByName")
+const getRecipesByName = require("./getRecipesByName");
+const getRecipesByCategory = require("./getRecipesByCategory");
+
 
 
 module.exports = {
-    getCategories: ctrlWrapper(getCategories),
+  getCategories: ctrlWrapper(getCategories),
   getRecipeById: ctrlWrapper(getRecipeById),
   getPopularRecipes: ctrlWrapper(getPopularRecipes),
   getMainPageRecipes: ctrlWrapper(getMainPageRecipes),
-  getRecipesByName: ctrlWrapper(getRecipesByName)
+  getRecipesByName: ctrlWrapper(getRecipesByName),
+  getRecipesByCategory: ctrlWrapper(getRecipesByCategory)
 };
