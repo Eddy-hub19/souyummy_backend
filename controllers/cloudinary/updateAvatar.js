6e6a3560ddf3e820d71251b329ca66368c70e63a
Alexander <Alexyaroshovets300687@gmail.com>
2023-04-03 22:48:45 +0300

    changename

--- controllers/cloudinary/updateAvatar.js
+++ controllers/cloudinary/updateAvatar.js
@@ -3,11 +3,12 @@ const cloudinary = require("cloudinary").v2;
 const { User } = require("../../models");
 
 const updateAvatar = async (req, res) => {
+  const { name } = req.body;
   const { _id } = req.user;
   const result = await cloudinary.uploader.upload(req.file.path);
   const updatedAvatarUrl = result.secure_url;
   console.log(updatedAvatarUrl);
-  await User.findByIdAndUpdate(_id, { avatarURL: updatedAvatarUrl });
+  await User.findByIdAndUpdate(_id, { avatarURL: updatedAvatarUrl, name: name });
   res.json(updatedAvatarUrl);
 };
 module.exports = { updateAvatar: ctrlWrapper(updateAvatar) };
