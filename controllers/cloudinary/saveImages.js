edafc4ae0154bb171101bca44a05a89359b47c34
Alexander <Alexyaroshovets300687@gmail.com>
2023-04-04 12:34:48 +0300

    fixroutes

--- controllers/cloudinary/saveImages.js
+++ controllers/cloudinary/saveImages.js
@@ -3,17 +3,6 @@ const cloudinary = require("cloudinary").v2;
 
 const saveImages = async (req, res) => {
   const result = await cloudinary.uploader.upload(req.file.path);
-  console.log(req._id);
   res.json(result);
 };
 module.exports = { saveImages: ctrlWrapper(saveImages) };
-
-//   const timestamp = Math.round(new Date().getTime() / 1000);
-//   const signature = cloudinary.utils.api_sign_request(
-//     {
-//       timestamp,
-//       folder: "menu",
-//       public_id: `menu-${timestamp}`,
-//     },
-//     API_SECRET
-//   );
