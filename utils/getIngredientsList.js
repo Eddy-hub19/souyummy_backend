2e45eefac028262b899ad348faa6e7d3e2ba05c4
EduardSekan <sekaneduard@gmail.com>
2023-04-08 00:02:00 +0300

    fix token

--- utils/getIngredientsList.js
+++ utils/getIngredientsList.js
@@ -5,7 +5,7 @@ const HttpError = require("../routes/errors/HttpErrors");
 const getIngredientsList = async (token) => {
   const res = await fetch(`${BASE_URL}/ingredients/list`, {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `${token}`,
     },
   });
   const data = await res.json();
